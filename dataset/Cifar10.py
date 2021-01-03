b0f7e04a775ccd643686a699aa0cd09cc920b99c
sianazhekova <siana.zhekova10@gmail.com>
2021-01-03 15:23:14 +0000

    Fixed some bugs

--- dataset/Cifar10.py
+++ dataset/Cifar10.py
@@ -16,11 +16,11 @@ class CIFAR10:
     train_data_augmentation = tf.keras.Sequential([
         layers.experimental.preprocessing.RandomCrop(32, 32),  #no option for padding in tf
         layers.experimental.preprocessing.RandomFlip(mode="horizontal"),
-        layers.experimental.preprocessing.Normalization(mean=[0.4914, 0.4822,0.4465], variance=[0.2023, 0.1994, 0.2010])
+        #tf.keras.preprocessing.image.ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
     ])
 
-    valid_data_augmentation = tf.keras.Sequemtial([
-        layers.experimental.preprocessing.Normalization(mean=[0.4914, 0.4822,0.4465], variance=[0.2023, 0.1994, 0.2010])
+    valid_data_augmentation = tf.keras.Sequential([
+        #tf.keras.preprocessing.image.ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
     ])
 
     @staticmethod
@@ -42,27 +42,30 @@ class CIFAR10:
         return X_train, X_valid, X_test, y_train, y_valid, y_test
     
     @staticmethod
-    def prepare_cifar10_data_augment():
-        X_train, y_train, X_test, y_test = datasets.cifar10.load_data()
+    def prepare_cifar10_data_augment(data_augment, batch_size):
+        (X_train, y_train), (X_test, y_test) = datasets.cifar10.load_data()
+        X_train, y_train, X_test, y_test = X_train.batch(batch_size), y_train.batch(batch_size), X_test.batch(batch_size), y_test.batch(batch_size)
         X_train, X_valid = X_train[:-5000], X_train[-5000:]
         y_train, y_valid = y_train[:-5000], y_train[-5000:]
         
         train_ds = zip(X_train, y_train)
         valid_ds = zip(X_valid, y_valid)
         test_ds = zip(X_test, y_test)
-
+        """
         train_ds = train_ds.batch(batch_size)
         valid_ds = valid_ds.batch(batch_size)
         test_ds = test_ds.batch(batch_size)
+        """
 
-        train_ds = train_ds.map(lambda x, y : (train_data_augmentation(x), y), num_parallel_calls=AUTOTUNE)
-        valid_ds = valid_ds.map(lambda x, y : (valid_data_augmentation(x), y), num_parallel_calls=AUTOTUNE)
+        if data_augment:
+            train_ds = train_ds.map(lambda x, y : (train_data_augmentation(x), y), num_parallel_calls=AUTOTUNE)
+            valid_ds = valid_ds.map(lambda x, y : (valid_data_augmentation(x), y), num_parallel_calls=AUTOTUNE)
 
         return train_ds.prefetch(buffer_size=AUTOTUNE), valid_ds.prefetch(buffer_size=AUTOTUNE), test_ds.prefetch(buffer_size=AUTOTUNE)
     
-    @staticmethod
-    def import_data_augment():
-        train_ds, valid_ds, test_ds = prepare_cifar10_data_augment()
+    @classmethod
+    def import_data(cls,data_augment=False):
+        train_ds, valid_ds, test_ds = cls.prepare_cifar10_data_augment(data_augment, cls.batch_size)
         X_train, y_train = ([x for x, y in train_ds], [y for x, y in train_ds])
         X_valid, y_valid = ([x for x, y in valid_ds], [y for x, y in valid_ds])
         X_test, y_test = ([x for x, y in test_ds], [y for x, y in test_ds])
