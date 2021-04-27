bf8ae79ef257f0be59dabdc86ef097ca204d7a6d
sianazhekova <siana.zhekova10@gmail.com>
2021-04-27 16:29:30 +0100

    Added axis=1 to lines 226 and 228

--- models/icarl_nn.py
+++ models/icarl_nn.py
@@ -223,9 +223,9 @@ class iCaRL(ModuleNN):
         for k in range(1, m+1):
             argmin_val = sys.maxsize
             pk = None
-            exemplar_features_sum = tf.math.reduce_sum([tf.math.l2_normalize(feature_map.predict(tf.expand_dims(p, axis=0))[0]) for p in P_list[:k]], axis=0) # Check this !!!
+            exemplar_features_sum = tf.math.reduce_sum([tf.math.l2_normalize(feature_map.predict(tf.expand_dims(p, axis=0))[0], axis=1) for p in P_list[:k]], axis=0) # Check this !!!
             for enum_i, x in enumerate(X_set[label]):
-                abs_diff = abs(mu - tf.math.l2_normalize(1/k * (feature_map_table[enum_i] + exemplar_features_sum)))   # CHECK THIS !!!
+                abs_diff = abs(mu - tf.math.l2_normalize(1/k * (feature_map_table[enum_i] + exemplar_features_sum)), axis=1)   # CHECK THIS !!!
                 print(f"The abs_diff is {abs_diff} and its shape is {abs_diff.shape}")
                 if abs_diff < argmin_val:
                     argmin_val = abs_diff
